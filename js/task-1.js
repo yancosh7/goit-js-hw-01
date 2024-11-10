5864c6e7785bec4ed41046fcdc3599d3b6ef9609
yancosh7 <yancosh@gmail.com>
2024-11-10 18:31:26 +0200

    fix js

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  const makeTransaction = (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-  return makeTransaction;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`
 }
+
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,10 @@ function getElementWidth(content, padding, border) {
     content = Number.parseFloat(content);
     padding = Number.parseFloat(padding) * 2;
     border = Number.parseFloat(border) * 2;
-    const boxSizing = content + padding + border;
-    
-    return boxSizing;
+    const boxsizing = content + padding + border;
+    return boxsizing;
 }
+
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
